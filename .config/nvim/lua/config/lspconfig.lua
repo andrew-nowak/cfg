83d68e2b7aa1f23354f806d9dee1ec3b2b5770e6
Andrew Nowak <andrew@nowak.me.uk>
2023-02-16 12:02:22 +0000

    disable autoreformat on save

--- .config/nvim/lua/config/lspconfig.lua
+++ .config/nvim/lua/config/lspconfig.lua
@@ -1,13 +1,13 @@
 local nvim_lsp = require 'lspconfig'
 local on_attach = function(client, bufnr)
   -- format on save
-  if client.server_capabilities.documentFormattingProvider then
-    vim.api.nvim_create_autocmd("BufWritePre", {
-      group = vim.api.nvim_create_augroup("Format", { clear = true }),
-      buffer = bufnr,
-      callback = function() vim.lsp.buf.formatting_seq_sync() end
-    })
-  end
+  --if client.server_capabilities.documentFormattingProvider then
+  --  vim.api.nvim_create_autocmd("BufWritePre", {
+  --    group = vim.api.nvim_create_augroup("Format", { clear = true }),
+  --    buffer = bufnr,
+  --    callback = function() vim.lsp.buf.formatting_seq_sync() end
+  --  })
+  --end
   vim.diagnostic.config({
     virtual_text = false
   })
