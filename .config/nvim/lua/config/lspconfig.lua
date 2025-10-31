c767f949dec9f9257afc0a19fb62940d429aaa11
Andrew Nowak <andrew.nowak@guardian.co.uk>
2025-10-31 18:48:36 +0000

    nvim: lspconfig deprecations

--- .config/nvim/lua/config/lspconfig.lua
+++ .config/nvim/lua/config/lspconfig.lua
@@ -1,4 +1,3 @@
-local nvim_lsp = require 'lspconfig'
 local on_attach = function(client, bufnr)
   -- format on save
   --if client.server_capabilities.documentFormattingProvider then
@@ -17,47 +16,43 @@ end
 
 -- TypeScript
 --TODO 
-nvim_lsp.ts_ls.setup {
+vim.lsp.config('ts_ls', {
   on_attach = on_attach,
-  root_dir = nvim_lsp.util.root_pattern("package.json"),
+  root_dir = vim.fs.root("package.json"),
   filetypes = { "typescript", "typescriptreact", "typescript.tsx" },
   cmd = { "typescript-language-server", "--stdio" },
   single_file_support = false
-}
+})
+vim.lsp.enable('ts_ls')
 
-nvim_lsp.denols.setup {
-  on_attach = on_attach,
-  root_dir = nvim_lsp.util.root_pattern("deno.json", "deno.jsonc"),
-}
+vim.lsp.enable('gopls')
 
-nvim_lsp.gopls.setup{}
+vim.lsp.enable('rust_analyzer')
 
-nvim_lsp.rust_analyzer.setup{}
-
-nvim_lsp.clangd.setup{}
-
-nvim_lsp.eslint.setup {
+vim.lsp.config('eslint', {
   on_attach = function(client, bufnr)
     vim.api.nvim_create_autocmd("BufWritePre", {
       buffer = bufnr,
       command = "EslintFixAll",
     })
   end,
-  root_dir = nvim_lsp.util.root_pattern("package.json"),
-}
+  root_dir = vim.fs.root("package.json"),
+})
+vim.lsp.enable('eslint')
 
-nvim_lsp.svelte.setup{}
+vim.lsp.enable('svelte')
 
-nvim_lsp.hls.setup {
+vim.lsp.config('hls', {
   filetypes = {'haskell', 'lhaskell', 'cabal' }
-}
+})
+vim.lsp.enable('hls')
 
-nvim_lsp.ocamllsp.setup{}
 
 
 vim.cmd [[
 nnoremap <silent> gd          <cmd>lua vim.lsp.buf.definition()<CR>
 nnoremap <silent> K           <cmd>lua vim.lsp.buf.hover()<CR>
+nnoremap <silent> L           <cmd>lua vim.diagnostic.open_float()<CR>
 nnoremap <silent> gi          <cmd>lua vim.lsp.buf.implementation()<CR>
 nnoremap <silent> gr          <cmd>lua vim.lsp.buf.references()<CR>
 nnoremap <silent> gds         <cmd>lua vim.lsp.buf.document_symbol()<CR>
@@ -66,7 +61,6 @@ nnoremap <silent> <leader>rn  <cmd>lua vim.lsp.buf.rename()<CR>
 nnoremap <silent> <leader>rf  <cmd>lua vim.lsp.buf.format { async = true }<CR>
 nnoremap <silent> <leader>a  <cmd>lua vim.lsp.buf.code_action()<CR>
 nnoremap <silent> <leader>ws  <cmd>lua require'metals'.worksheet_hover()<CR>
-nnoremap <silent> <leader>diag   <cmd>lua require'metals'.open_all_diagnostics()<CR>
 nnoremap <silent> <space>d    <cmd>lua vim.lsp.diagnostic.set_loclist()<CR>
 nnoremap <silent> [c          <cmd>lua vim.lsp.diagnostic.goto_prev { wrap = false }<CR>
 nnoremap <silent> ]c          <cmd>lua vim.lsp.diagnostic.goto_next { wrap = false }<CR>
--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -9,10 +9,6 @@ vim.cmd [[packadd packer.nvim]]
 packer.startup(function(use)
   use 'wbthomason/packer.nvim'
 
-  --use { 'junegunn/fzf.vim', requires = {'junegunn/fzf'}, config = function() vim.cmd [[
-  --  source setup-fzf.vim
-  --]] end }
-
   use { 'nvim-telescope/telescope.nvim', tag = '0.1.5',
     requires = {
       'nvim-lua/plenary.nvim',
@@ -23,7 +19,6 @@ packer.startup(function(use)
 
   use { 'neovim/nvim-lspconfig', tag = 'v2.4.0', -- TODO unpin
     config = [[require 'config.lspconfig']],
-    --run = [[npm install -g typescript-language-server@1.0.0]]
   }
 
   use { 'hrsh7th/nvim-cmp', config = [[require 'config.cmp']],
