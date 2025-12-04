e3e91b4370f8ea25e001011abca330ae6f68f99b
Andrew Nowak <andrew.nowak@guardian.co.uk>
2025-12-04 12:40:58 +0000

    nvim: add neogit

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,5 +1,21 @@
 return {
 
+  {
+    "NeogitOrg/neogit",
+    lazy = true,
+    dependencies = {
+      "nvim-lua/plenary.nvim",         -- required
+      "sindrets/diffview.nvim",        -- optional - Diff integration
+
+      -- Only one of these is needed.
+      "nvim-telescope/telescope.nvim", -- optional
+    },
+    cmd = "Neogit",
+    keys = {
+      { "<leader>gg", "<cmd>Neogit<cr>", desc = "Show Neogit UI" }
+    }
+  },
+
   { 'nvim-telescope/telescope.nvim', tag = '0.1.5',
     dependencies = {
       'nvim-lua/plenary.nvim',
