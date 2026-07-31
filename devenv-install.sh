18c2cda99c6d88b36792b1f556400666f5dfa4e4
Andrew Nowak <andrew.nowak@guardian.co.uk>
2026-07-31 17:07:15 +0100

    devenv: install more expected packages

--- devenv-install.sh
+++ devenv-install.sh
@@ -8,10 +8,18 @@ function config {
 mkdir -p $HOME/.config-backup
 config checkout
 if [ $? = 0 ]; then
-  echo "Checked out config.";
+  echo "Checked out config."
   else
     echo "Backing up pre-existing dot files.";
     config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} $HOME/.config-backup/{}
-fi;
+fi
 config checkout
 config config status.showUntrackedFiles no
+
+sudo apt update
+sudo apt install -y neovim fzf ripgrep starship
+
+sudo chsh -s /usr/bin/zsh
+
+nvim --headless "+Lazy! sync" +qa
+
